87d6a410393f50face9dd4964eba44e2ef2dd125
wagnerk78 <wagnerkroiss@gmail.com>
2023-07-03 12:03:22 -0300

    03th July

--- src/main/java/com/algaworks/algatransito/api/controller/ProprietarioController.java
+++ src/main/java/com/algaworks/algatransito/api/controller/ProprietarioController.java
@@ -3,11 +3,9 @@ package com.algaworks.algatransito.api.controller;
 import com.algaworks.algatransito.domain.model.Proprietario;
 import com.algaworks.algatransito.domain.repository.ProprietarioRepository;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -26,32 +24,57 @@ public class ProprietarioController {
     }*/
 
     //  @PersistenceContext
-   // private EntityManager manager;
+    // private EntityManager manager;
 
     @GetMapping
-    public List<Proprietario> lista(){
+    public List<Proprietario> lista() {
     /*TypedQuery<Proprietario> query = manager.createQuery("from Proprietario", Proprietario.class);
     return query.getResultList();*/
 
-    return proprietarioRepository.findByNome("Maria");
-            // proprietarioRepository.findAll();
-           }
+        return proprietarioRepository.findAll();
+    }
 
     @GetMapping("/{proprietarioId}")
-    public ResponseEntity<Proprietario> buscar(@PathVariable Long proprietarioId){
+    public ResponseEntity<Proprietario> buscar(@PathVariable Long proprietarioId) {
         return proprietarioRepository.findById(proprietarioId)
                 .map(ResponseEntity::ok)
                 .orElse(ResponseEntity.notFound().build());
 
-
-
-
-
        /* if(proprietario.isPresent()) {
             return ResponseEntity.ok(proprietario.get());
         }
-
         return ResponseEntity.notFound().build();*/
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Proprietario adicionar(@RequestBody Proprietario proprietario) {
+        return proprietarioRepository.save(proprietario);
+    }
+
+    @PutMapping("/{proprietarioId}")
+    public ResponseEntity<Proprietario> atualizar(@PathVariable Long proprietarioId, @RequestBody Proprietario proprietario) {
+
+        if (!proprietarioRepository.existsById(proprietarioId)) {
+            return ResponseEntity.notFound().build();
+        }
+
+        proprietario.setId(proprietarioId);
+        Proprietario proprietarioAtualizado = proprietarioRepository.save(proprietario);
+
+        return ResponseEntity.ok(proprietarioAtualizado);
+    }
+
+
+    @DeleteMapping("/{proprietarioId}")
+    public ResponseEntity<Void> remover(@PathVariable Long proprietarioId){
+
+        if (!proprietarioRepository.existsById(proprietarioId)) {
+            return ResponseEntity.notFound().build();
+        }
+
+        proprietarioRepository.deleteById(proprietarioId);
+        return ResponseEntity.noContent().build();
     }
 
 
